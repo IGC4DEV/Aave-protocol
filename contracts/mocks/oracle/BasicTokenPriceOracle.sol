d583f71cbce2e95bb1ff22774159eddd880ff597
Alexandre Perez <aperez@theblockchainxdev.com>
2022-11-04 16:13:28 +0100

    add BasicTokenPriceOracle

---
+++ contracts/mocks/oracle/BasicTokenPriceOracle.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title CASTPrice
+ * @dev Store & retrieve price of Casino Token
+ */
+contract BasicTokenPriceOracle is Ownable {
+
+    uint256 price;
+
+    /**
+     * @dev Store value in variable
+     * @param _price value to store
+     */
+    function setPrice(uint256 _price) public onlyOwner {
+        price = _price;
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'price'
+     */
+    function getPrice() public view returns (uint256){
+        return price;
+    }
+}
